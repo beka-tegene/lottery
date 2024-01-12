af817f6b61f36b9aea68e67e7aa949eafed3d617
beka-tegen <bekategene177@gmail.com>
2024-01-12 16:14:10 +0300

    modify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import Random from "./Components/Random";
+import Form from "./Components/Form";
 function App() {
   return (
-    <div style={{ height: "100vh", width: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", background:"linear-gradient(45deg , #503C3C 50%, #3E3232 50%)" }}>
-      <Random />
+    <div style={{ height: "100vh", width: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", background: "linear-gradient(45deg , #503C3C 50%, #3E3232 50%)" }}>
+      <Routes>
+        <Route path="/" element={<Random />} />
+        <Route path="/register" element={<Form />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Random.jsx
+++ src/Components/Random.jsx
@@ -3,16 +3,6 @@ import { css } from '@emotion/react';
 import { ClipLoader } from 'react-spinners';
 import { motion } from 'framer-motion';
 import logo from '../Image/logo.gif'
-const DUMMYPHONE = [
-  { phone: '0941729595' },
-  { phone: '0941729535' },
-  { phone: '0941729525' },
-  { phone: '0922857673' },
-  { phone: '0941729295' },
-  { phone: '0941723295' },
-  { phone: '0941724595' },
-  { phone: '0943459595' },
-];
 
 const Random = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
@@ -20,18 +10,28 @@ const Random = () => {
   const [spinning, setSpinning] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      if (spinning) {
-        const randomIndex = Math.floor(Math.random() * DUMMYPHONE.length);
-        const newPhoneNumber = DUMMYPHONE[randomIndex].phone;
-        setPhoneNumber(newPhoneNumber);
-        setSpinning(false);
-        setLoading(false);
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/v1/lotto/allPhoneNumbers");
+        const data = await response.json();
+
+        if (spinning && data.phoneNumbers && data.phoneNumbers.length > 0) {
+          const randomIndex = Math.floor(Math.random() * data.phoneNumbers.length);
+          const newPhoneNumber = data.phoneNumbers[randomIndex];
+          setPhoneNumber(newPhoneNumber);
+          setSpinning(false);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-    }, 2000);
+    };
+
+    const timer = setTimeout(fetchData, 2000);
 
     return () => clearTimeout(timer);
   }, [spinning]);
+
 
   const generateRandomNumber = () => {
     setLoading(true);
@@ -43,24 +43,25 @@ const Random = () => {
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      style={{textAlign:"center",width: '400px',
-      borderRadius: '4px',background:"#7E6363", padding: 16,}}
-      >
+      style={{
+        textAlign: "center", width: '400px',
+        borderRadius: '4px', background: "#7E6363", padding: 16,
+      }}
+    >
       <img src={logo} alt="logo" style={{ width: "100px" }} />
       <div
         style={{
-          
+
           color: '#272727',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
-         
-          
-        }}
 
+
+        }}
       >
-        <div style={{ display: 'flex', flexDirection: 'column', gap: 16, textAlign: 'center' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 16, alignItems: "center" }}>
           <h2 style={{ fontSize: '24px', fontWeight: 'bold' }}>DAN Lottery</h2>
           <p style={{ fontSize: '16px', color: '#A0A0A0' }}>Click the button to generate a random phone number</p>
           <button
@@ -73,6 +74,7 @@ const Random = () => {
               fontSize: '20px',
               fontWeight: 'bold',
               color: '#FFFFFF',
+              width: "100%"
             }}
             onClick={generateRandomNumber}
             disabled={loading || spinning}
@@ -83,20 +85,21 @@ const Random = () => {
               'Spin Wheel'
             )}
           </button>
-
-          <motion.h1
-            style={{
-              fontSize: '28px',
-              fontWeight: 'bold',
-              color: loading ? '#A0A0A0' : '#00FF00',
-              marginTop: '8px',
-            }}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            {spinning ? 'Spinning...' : phoneNumber}
-          </motion.h1>
+          <div style={{ width: "200px", height: "6vh", maxWidth: "200px", maxHeight: "7vh", border: "1px solid #019328", display: 'flex', flexDirection: 'column', justifyContent: "center", borderRadius: '4px', overflow: "hidden" }}>
+            <motion.h1
+              style={{
+                fontSize: '28px',
+                fontWeight: 'bold',
+                color: loading ? '#A0A0A0' : '#00FF00',
+                marginTop: '8px',
+              }}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              {spinning ? 'Spinning...' : phoneNumber}
+            </motion.h1>
+          </div>
         </div>
       </div>
     </motion.div>
